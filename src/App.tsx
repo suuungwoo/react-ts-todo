6ec0bf7ec955dfe16a5d55c388485b361f1cc94c
suuungwoo <suuungwoo@gmail.com>
2019-06-09 21:13:28 +0900

    完了状態切替をボタンに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {FunctionComponent, useState} from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import './App.css';
 
 interface Todo {
@@ -34,34 +34,53 @@ const App: FunctionComponent = () => {
           onChange={e => setText(e.target.value)}
           value={text}
         />
-        <button onClick={addTodo}>add</button>
+        <button disabled={!text} onClick={addTodo}>
+          add
+        </button>
       </div>
       <div>
         <button onClick={() => setFilter('all')}>ALL</button>
         <button onClick={() => setFilter('completed')}>COMPLETED</button>
         <button onClick={() => setFilter('notCompleted')}>NOT COMPLETED</button>
       </div>
-      <Ul>
-        {todos
-          .filter(
-            todo =>
-              (filter === 'all' && todo) ||
-              (filter === 'completed' && todo.completed) ||
-              (filter === 'notCompleted' && !todo.completed)
-          )
-          .map(todo => (
-            <li key={todo.id}>
-              <input type="checkbox" onClick={toggleCompleted(todo.id)} />
-              {todo.text}
-            </li>
-          ))}
-      </Ul>
+      {todos.length ? (
+        <Ul>
+          {todos
+            .filter(
+              todo =>
+                (filter === 'all' && todo) ||
+                (filter === 'completed' && todo.completed) ||
+                (filter === 'notCompleted' && !todo.completed)
+            )
+            .map(todo => (
+              <Li key={todo.id} completed={todo.completed}>
+                <button onClick={() => toggleCompleted(todo.id)}>完了</button>
+                {todo.text}
+              </Li>
+            ))}
+        </Ul>
+      ) : (
+        <p>タスクがありません</p>
+      )}
     </>
   );
 };
 
 export default App;
 
-export const Ul = styled.ul`
+const Ul = styled.ul`
   list-style-type: none;
+`;
+interface LiProps {
+  completed: boolean;
+}
+const Li = styled.li`
+  ${(props: LiProps) =>
+    props.completed
+      ? css`
+          color: blue;
+        `
+      : css`
+          color: red;
+        `}
 `;
